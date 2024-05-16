224555d6e9bbf82ffecdb8a2f4ee48d119829ae5
Meowveloper <zwekhantaung22.tgi@gmail.com>
2024-05-17 01:25:11 +0700

    Ep 88

--- src/pages/BlogDetail.js
+++ src/pages/BlogDetail.js
@@ -1,11 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
+import { useEffect } from "react";
 
 export default function BlogDetail() {
     
     const params = useParams();
     
     const { data : blog, loading, error } = useFetch(`http://localhost:3001/blogs/${params.id}`);
+
+    const navigate = useNavigate();
+    useEffect(function () {
+        if(error) setTimeout(() => { navigate('/') }, 2000);
+    }, [error, navigate])
     return (
         <div>
             { error && <div>{error}</div>}
